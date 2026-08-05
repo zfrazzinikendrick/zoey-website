35cbb816dbe9cf1e77a91eb98a9d059d4c99f397
frazz <frazzdrick@gmail.com>
2026-08-05 19:19:01 -0400

    added main pages and setup uv project env

--- app.py
+++ app.py
@@ -2,7 +2,34 @@ import streamlit as st
 import numpy as np
 import datetime
 
-st.title("Zoey's Website")
+
+def home_page():
+    st.title(":green[Zoey's Website]")
+
+def resume_page():
+    st.title("Resume Page")
+    st.markdown("supporting text... bla bla bla...")
+
+def project_page():
+    st.title("Projects")
+    st.markdown("placeholder text.... bla bla bla...")
 
 
+#page = st.navigation(
+#    {"Home": home_page,
+#    "Resume": resume_page,
+#    "Projects": project_page},
+#    position="top",
+#    expanded=True
+#)
+pages = [st.Page(home_page, title="Home"), 
+     st.Page(resume_page, title="Resume"), 
+     st.Page(project_page, title="Projects")
+    ]
+page = st.navigation(
+    pages,
+    position="top",
+    expanded=True
+)
 
+page.run()
